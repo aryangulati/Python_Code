4a596f39b80becd503a532525de9006b43df021a
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-06-22 13:35:52 +0530

    .

--- 6.)Recognition/Face_recognition.py
+++ 6.)Recognition/Face_recognition.py
@@ -18,7 +18,7 @@ import cv2
 
 
 
-dataset_path = "./data/"
+dataset_path = "./data1/"
 
 face_data = []
 labels = []
@@ -29,7 +29,7 @@ for fx in os.listdir(dataset_path):
         
         face_item = np.load(dataset_path + fx)
         # print(face_item.shape)
-        print("loaded " , l)
+        print("loaded " , l) #so it can tell which all have loaded
         
         face_data.append(face_item)
         # appending labels #times => faces
@@ -42,4 +42,84 @@ Y = np.array(labels)
 
 print(X.shape)
 print(Y.shape)
+
+# KNN CODE 
+def distance(pA, pB):
+    return np.sum((pB - pA)**2)**0.5
+
+
+def kNN(X, y, x_query, k = 5):
+
+
+	"""
+	X - > (m, 30000)  np array
+	y - > (m,) np array
+	x_query -> (1,30000) np array
+	k -> scaler  int
+	do knn for classification
+	"""
+	m = X.shape[0]
+	distances = []
+
+	for i in range(m):
+		dis = distance(x_query, X[i])
+		distances.append((dis, Y[i]))
+	    
+	distances = sorted(distances)
+	distances = distances[:k]
+
+	distances = np.array(distances)
+	labels = distances[:, 1]
+
+
+	uniq_label, counts = np.unique(labels, return_counts=True)
+
+	pred = uniq_label[counts.argmax()]
+
+
+	return pred
+
+# TEST FACE RECOG
+
+
+cam = cv2.VideoCapture(0)
+
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+
+
+
+while True:
+    ret, frame = cam.read()
+    if ret == False:
+        continue
+        
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # bgr -> grayscale conversion
+    faces = face_cascade.detectMultiScale(gray_frame, 1.3, 5)
+    
+    
+
+    for face in faces:
+        x, y, w, h = face
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+        
+        offset = 10
+        face_section = frame[y-offset : y+h+offset , x-offset : x+w+offset]
+        face_section = cv2.resize(face_section , (100 , 100))
+        
+        name = kNN(X, Y, face_section.reshape(1,-1)) #in x_query we have to pass face_section but we have to resize
+        #it into 1,100,100,3 so we can reshape or .flatten() int0 -1 or 30000
+        
+        cv2.putText(frame, name.title(), (x, y-10), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255), 2, cv2.LINE_AA)
+        #check .puText()? check docs
+        #putText(img, text, org, fontFace, fontScale, color[, thickness[, lineType[, bottomLeftOrigin]]])
+    
+    cv2.imshow("window", frame)
+
+    key = cv2.waitKey(1) # 1ms
+    if key == ord("q"):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
 
