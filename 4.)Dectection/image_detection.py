d9e2b17e1f3a9006e6f0b54ab06ecaf70998d860
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-10-15 21:41:44 +0530

    updated openCV

--- 4.)Dectection/image_detection.py
+++
@@ -1,8 +0,0 @@
-#simple opencv
-import cv2
-
-image=cv2.imread("bob.jpg")
-
-cv2.imshow("Bob The Builder",image)
-cv2.waitKey(1000) #it hold process execution for new window of img
-# and it contain millisecond parameter put 1000
